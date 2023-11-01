597c9b13467583d0c43b38c49de922add8a1ffac
siriusjbk <139738840+siriusjbk@users.noreply.github.com>
2023-11-01 10:32:59 +0900

    Update main.py
    코드 최적화 되었습니다.

--- main.py
+++ main.py
@@ -1,41 +1,38 @@
+#module
 import base64
 import pyperclip
 
-#decode
+#print
 print("엔터 누를시 자동으로 클립보드 데이터 디코드후 복사")
 print("인코드 데이터 복사후 엔터 누를시 자동으로 인코드 후 복사")
-print("혹은 e 입력시 자동으로 클리보드 데이터 인코드후 복사")
-_encode_=input(">>>")
+print("혹은 e 입력시 자동으로 클립보드 데이터 인코드후 복사")
 
-#decode clipboard
-if _encode_=='' :
-    code = pyperclip.paste()
-    code_bytes = code.encode('ascii')
+#decode
+while True :
+    _encode_=input(">>>")
 
-    decoded = base64.b64decode(code_bytes)
-    str = decoded.decode('UTF-8')
-    pyperclip.copy(str)
-    print(str)
-    print("가 복사 되었습니다.")
-    input("아무키나 누르면 종료")
+    #decode clipboard
+    if _encode_=='' :
+        str = pyperclip.paste()
+        bytes = str.encode('ascii')
+        result = base64.b64decode(bytes)
+        result_str = result.decode('UTF-8')
 
-#encode
-elif _encode_=='e':
-    str = pyperclip.paste()
-    bytes = str.encode('UTF-8')
-    result = base64.b64encode(bytes)
-    result_str = result.decode('ascii')
+    #encode
+    elif _encode_=='e':
+        str = pyperclip.paste()
+        bytes = str.encode('UTF-8')
+        result = base64.b64encode(bytes)
+        result_str = result.decode('ascii')
+
+    #encode_2
+    else :
+        str=_encode_
+        bytes = str.encode('UTF-8')
+        result = base64.b64encode(bytes)
+        result_str = result.decode('ascii')
+
+    #copy & print the result_str
     pyperclip.copy(result_str)
     print(result_str)
-    print("가 복사 되었습니다.")
-    input("아무키나 누르면 종료")
-
-else :
-    str=_encode_
-    bytes = str.encode('UTF-8')
-    result = base64.b64encode(bytes)
-    result_str = result.decode('ascii')
-    pyperclip.copy(result_str)
-    print(result_str)
-    print("가 복사 되었습니다.")
-    input("아무키나 누르면 종료")
+    print("is copied.")
